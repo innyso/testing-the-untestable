3fd501007c401bd23d73ddb5cdd7b09273e9ffa9
innyso <mini.inny@gmail.com>
2016-04-18 18:12:14 +1000

    just another update
    - make sure rack backend working
    - print out json from respond
    - add upstream for nginx

--- demo/backend/config.ru
+++ demo/backend/config.ru
@@ -1,1 +1,2 @@
-run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ['get rack\'d']] }
+require "json"
+run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, [env.to_json]] }
--- demo/spec/sample_spec.rb
+++ demo/spec/sample_spec.rb
@@ -3,8 +3,6 @@ require 'airborne'
 describe "sample test" do
   it 'should get something' do
     get 'http://www.iso-sample.com/sample'
-    headers.each do |header|
-      print header
-    end
+    print json_body
   end
 end
