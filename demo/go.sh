1785c470637208fa14db933822f3c964b57548f7
innyso <mini.inny@gmail.com>
2016-05-13 00:02:21 +1000

    fix test failure

--- demo/go.sh
+++ demo/go.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 
+:
 sudo docker-compose up -d
 
 #need to sleep in case result is not here yet
 sleep 2s
+
 
 exit_code=`sudo docker-compose ps -q spec | xargs sudo docker inspect -f '{{ .State.ExitCode }}'`
 echo $exit_code
