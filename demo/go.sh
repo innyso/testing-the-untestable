accb46de94e4d3ee9d2a4b33449924cc3aaa6f45
innyso <mini.inny@gmail.com>
2016-05-12 23:45:33 +1000

    need sudo for docker command

--- demo/go.sh
+++ demo/go.sh
@@ -5,7 +5,7 @@ sudo docker-compose up -d
 #need to sleep in case result is not here yet
 sleep 2s
 
-ret_code=sudo docker-compose ps -q spec | xargs docker inspect -f '{{ .State.ExitCode }}'
+ret_code=sudo docker-compose ps -q spec | xargs sudo docker inspect -f '{{ .State.ExitCode }}'
 echo $ret_code
 echo "========================="
 exit $ret_code
