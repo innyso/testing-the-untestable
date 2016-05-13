8cd7b876c0dd9e675dd6ef6a14a205b89a8413e1
innyso <mini.inny@gmail.com>
2016-05-13 13:35:07 +1000

    merge changes

--- demo/spec/sample_spec.rb
+++ demo/spec/sample_spec.rb
@@ -1,9 +1,9 @@
 require 'airborne'
 
 describe "sample test" do
-  it 'should redirect to www.iso-backend' do
-    get 'http://www.iso-sample.com/sample'
-    expect(json_body[:SERVER_NAME]).to eq "www.iso-backend.com"
+  it 'should redirect to www.testing-backend' do
+    get 'http://www.testing-sample.com/sample'
+    expect(json_body[:SERVER_NAME]).to eq "www.testing-backend.com"
   end
   it 'should return 404' do
     get 'http://www.iso-sample.com/sample123'
