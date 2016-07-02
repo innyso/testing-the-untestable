546c3808dadabeb2cb7a3a48e0026013815eb3de
innyso <mini.inny@gmail.com>
2016-07-03 01:19:55 +1000

    dont need sudo anymore for docker

--- demo/go.sh
+++ demo/go.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-sudo docker-compose stop
-sudo docker-compose up -d
+docker-compose stop
+docker-compose up -d
 
 #need to sleep in case result is not here yet
 sleep 2s
 
 #Get exit code from rspec container
-exit_code=`sudo docker-compose ps -q spec | xargs sudo docker inspect -f '{{ .State.ExitCode }}'`
+exit_code=`docker-compose ps -q spec | xargs sudo docker inspect -f '{{ .State.ExitCode }}'`
 exit $exit_code
 
