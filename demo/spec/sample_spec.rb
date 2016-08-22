a5afaa100ad19dbeab6851288636b6ef96cf47ba
innyso <mini.inny@gmail.com>
2016-08-22 16:35:38 +1000

    fail test

--- demo/spec/sample_spec.rb
+++ demo/spec/sample_spec.rb
@@ -5,4 +5,8 @@ describe "sample test" do
     get 'http://www.testing-sample.com/sample'
     expect(json_body[:SERVER_NAME]).to eq "www.testing-backend.com"
   end
+  it 'get a 404 if I go to sample123' do
+    get 'http://www.testing-sample.com/sample123'
+    expect_status 404
+  end
 end
