7d5870464417f70b5efd103fa85160241688cc59
innyso <mini.inny@gmail.com>
2016-09-19 21:22:54 +1000

    update slides and add new compliance test cases

--- demo/spec/compliance_spec.rb
+++ demo/spec/compliance_spec.rb
@@ -1,8 +1,8 @@
 require 'airborne'
 
-describe "compliance test" do
-  it "should not show server header" do
-   get 'http://www.testing-sample.com/sample'
-   expect(headers[:server]).not_to match(/.+\/\d+\.*\d*\.*\d*$/) 
+describe "compliance_test" do
+  it 'any reverse proxy should not include version in header' do
+    get 'http://www.testing-sample.com/sample'
+    expect(headers[:server]).not_to match(/.+\/\d+\.*\d*\.*\d*$/)
   end
 end
