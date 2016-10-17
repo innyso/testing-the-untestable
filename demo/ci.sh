fa7c903c6885dfa4bafb459fd5dd68612d3b4fa1
innyso <mini.inny@gmail.com>
2016-10-18 10:26:48 +1100

    update ci.sh to return correct exit code

--- demo/ci.sh
+++ demo/ci.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+docker-compose stop
 docker-compose up -d
+
+#need to sleep in case result is not here yet
+sleep 2s
+
+#Get exit code from rspec container
+exit_code=`docker-compose ps -q spec | xargs docker inspect -f '{{ .State.ExitCode }}'`
+
+#cleanup
+docker-compose stop
+yes | sudo docker-compose rm
+exit $exit_code
 
