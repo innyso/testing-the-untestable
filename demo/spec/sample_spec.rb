bb1257e8b2396fb026508aaa6882b2fe855a61fe
innyso <mini.inny@gmail.com>
2016-10-19 11:13:20 +1100

    making test break to see if concourse ci breaks

--- demo/spec/sample_spec.rb
+++ demo/spec/sample_spec.rb
@@ -5,4 +5,8 @@ describe "sample test" do
     get 'http://www.testing-sample.com/sample'
     expect(json_body[:SERVER_NAME]).to eq "www.testing-backend.com"
   end
+  it 'should redirect to www.testing-backend' do
+    get 'http://www.testing-sample.com/sample123'
+    expect_status(404)
+  end
 end
