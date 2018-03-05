38463c1cc7d9a1400b974ba0151d420ae1919ec1
innyso <mini.inny@gmail.com>
2018-03-05 18:32:53 +1100

    remove sudo

--- demo/go.sh
+++ demo/go.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-sudo docker-compose stop
-sudo docker-compose up -d
+docker-compose stop
+docker-compose up -d
 
 #need to sleep in case result is not here yet
 sleep 2s
 
 #Get exit code from rspec container
-exit_code=`sudo docker-compose ps -q spec | xargs sudo docker inspect -f '{{ .State.ExitCode }}'`
+exit_code=`docker-compose ps -q spec | xargs docker inspect -f '{{ .State.ExitCode }}'`
 
 #cleanup
-sudo docker-compose stop
+docker-compose stop
 yes | sudo docker-compose rm
 exit $exit_code
 
